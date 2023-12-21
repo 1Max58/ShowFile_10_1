5c1c463801c5b4e183cfa14f94d62fc8a1eda8a9
1Max58 <mkorsun1@stu.vistula.edu.pl>
2023-12-21 10:45:31 +0200

    Initial commit

---
+++ src/com/example/showfile/ShowFile.java
@@ -0,0 +1,36 @@
+package com.example.showfile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ShowFile {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis;
+        int bajt;
+
+        try {
+            fis = new FileInputStream("test123.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return;
+        }
+
+        try {
+            do {
+                bajt = fis.read();
+                if (bajt != -1) System.out.println((char) bajt);
+            } while (bajt != -1);
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+
+        try {
+            fis.close();
+        } catch (IOException e) {
+            System.out.println("Error closing file.");
+        }
+    }
+}
